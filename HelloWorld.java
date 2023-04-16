b05a42b8bd293bb9ede5ba14fe3b6de9aeec6400
Bhavadharani <gkbavatharani2002@gmail.com>
2023-04-16 13:16:05 +0530

    my first java code

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        int a = 10;
+        int b = 100;
+        int c;
+
+        c = b / a;
+
+        System.out.println("Hello, Dhaarani!");
+        System.out.println(c);
+    }
+}
